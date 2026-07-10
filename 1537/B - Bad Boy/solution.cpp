8785fab296ad3c42ae38bc3f0d05688bbdede9bd
Veer <157895177+Veerhooda@users.noreply.github.com>
2026-07-10 18:05:39 +0530

    Add Bad Boy [B] from Codeforces

---
+++ 1537/B - Bad Boy/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        int m;
+        int i;
+        int j;
+        cin>>n;
+        cin>>m;
+        cin>>i;
+        cin>>j;
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+    }
+}
