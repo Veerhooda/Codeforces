a9fb30cf4fc52b1247d5ed0d68388dbfbf2d8fa5
Veer <157895177+Veerhooda@users.noreply.github.com>
2026-07-10 18:05:16 +0530

    Add Binary Array Game [A] from Codeforces

---
+++ 2183/A - Binary Array Game/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        if(a[0]==1 or a[n-1]==1){
+            cout<<"Alice"<<endl;
+        }
+        else{
+            cout<<"Bob"<<endl;
+        }
+    }
+}
