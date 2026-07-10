2fc2dada5d32a5ad993358c6ffbce352d8d1b6ff
Veer <157895177+Veerhooda@users.noreply.github.com>
2026-07-10 17:59:25 +0530

    Add Tab Closing [B] from Codeforces

---
+++ 2166/B - Tab Closing/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+    int a;
+    cin>>a;
+    int b;
+    cin>>b;
+    int n;
+    cin>>n;
+        
+        if(a/n<b && a!=b){
+            cout<<2<<endl;
+            
+        }
+        
+        else{
+            cout<<1<<endl;
+        }
+    
+    
+    
+    }
+ 
+}
