828655804204fabd895f3b949624403c4ff9b095
Veer <157895177+Veerhooda@users.noreply.github.com>
2026-07-10 18:04:07 +0530

    Add NIT Destroys the Universe [B] from Codeforces

---
+++ 1696/B - NIT Destroys the Universe/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+                int ans=0;
+                for(int i=0;i<n;i++){
+                    if(a[i]!=0 && (i==0||a[i-1]==0)){
+                        ans++;
+                    }
+                }
+                cout<<min(ans,2)<<endl;
+            }
+        
+    
+}
