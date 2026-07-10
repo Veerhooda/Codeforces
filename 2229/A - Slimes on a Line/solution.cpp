911337f059fd1d48e24627c1f044a67beae26d51
Veer <157895177+Veerhooda@users.noreply.github.com>
2026-07-10 18:11:03 +0530

    Add Slimes on a Line [A] from Codeforces

---
+++ 2229/A - Slimes on a Line/solution.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#define int long long
+using namespace std;
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        int count = (a[n-1]-a[0]+1)/2;
+        cout<<count<<endl;
+    }
+    return 0;
+}
