d88b15a2b165a58d0ee58a7a379d9c89b9a44e52
Veer <157895177+Veerhooda@users.noreply.github.com>
2026-07-10 18:01:19 +0530

    Add Blackslex and Password [A] from Codeforces

---
+++ 2179/A - Blackslex and Password/solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int k;
+        int x;
+        cin>>k;
+        cin>>x;
+        cout<<(k*x)+1<<endl;
+    }
+}
