5fdddb15d1baf5c6f97ece0e0cd7f27986e62794
Veer <157895177+Veerhooda@users.noreply.github.com>
2026-07-10 18:06:44 +0530

    Add AB Balance [A] from Codeforces

---
+++ 1606/A - AB Balance/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int n=s.size();
+        if(s[0]==s[n-1]){
+            cout<<s<<endl;
+        }
+        else{
+            s[n-1]=s[0];
+            cout<<s<<endl;
+        }
+    }
+}
