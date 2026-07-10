1387fe2157c1e6caadafa70c6f4cbe3cc144395f
Veer <157895177+Veerhooda@users.noreply.github.com>
2026-07-10 18:03:51 +0530

    Add Not Dividing [B] from Codeforces

---
+++ 1794/B - Not Dividing/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            if(a[i]==1){
+                a[i]++;
+            }
+        }
+        for(int i=0;i<n-1;i++){
+                if(a[i+1]%a[i]==0){
+                    a[i+1]++;
+                }
+        }
+        for(int i=0;i<n;i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
